ef60fae51d86f88550ad6abb49306cf7ea9b4810
Lorenzo Ercole s277956 <s277956@studenti.polito.i>
2021-01-11 09:22:02 +0100

    fix: aggiunti al progetto demo degli npc e dei grabbable

--- PoliTo Explore/Assets/Scripts/Spawner.cs
+++ PoliTo Explore/Assets/Scripts/Spawner.cs
@@ -16,7 +16,7 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)){
+        if (Input.GetKeyDown(KeyCode.E)){
             SpawnStudent();
         }
     }
