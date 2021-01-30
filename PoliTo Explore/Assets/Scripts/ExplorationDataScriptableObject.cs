cc3478814a0508759e9a95ad87e449652b19e584
Lorenzo Ercole s277956 <s277956@studenti.polito.i>
2021-01-30 13:36:46 +0100

    Aggiunti trigger e collider ai cestini(esterni non bellissimi), altri file

---
+++ PoliTo Explore/Assets/Scripts/ExplorationDataScriptableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ExplorationDataScriptableObject", order = 1)]
+public class ExplorationDataScriptableObject : ScriptableObject
+{
+
+
+    public void OnEnable()
+    {
+        Debug.Log("OnEnable");
+        
+    }
+
+    public void Awake()
+    {
+        Debug.Log("Awake");
+    }
+}
