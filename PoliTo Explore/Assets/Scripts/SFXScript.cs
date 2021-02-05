8fe75c1dadddef162b2a86f49dc79e4e3e9a9ebc
Vlad <s237362@studenti.polito.it>
2021-02-05 13:39:52 +0100

    aggiunto tetti

---
+++ PoliTo Explore/Assets/Scripts/SFXScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXScript : MonoBehaviour
+{
+    private AudioSource audioSrc;
+
+    [SerializeField]
+    private AudioClip sound;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        audioSrc.PlayOneShot(sound);
+    }
+
+}
