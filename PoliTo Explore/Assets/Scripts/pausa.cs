8430578f86c136dd68624ee5cd16cc7e4e3f6d6e
Giorgio <giorgios94@hotmail.it>
2021-02-08 18:50:20 +0100

    Menu_circa

---
+++ PoliTo Explore/Assets/Scripts/pausa.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausa : MonoBehaviour
+{
+    public static bool GiocoInPausa = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GiocoInPausa)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        } 
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GiocoInPausa = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GiocoInPausa = true;
+    }
+}
