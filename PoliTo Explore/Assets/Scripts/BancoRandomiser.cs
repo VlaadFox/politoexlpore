3598d2228be48891154d6d1a05ac22d30ec3be43
Lorenzo Ercole s277956 <s277956@studenti.polito.i>
2021-02-10 13:40:12 +0100

    aggiunto randomiser dei banchi in aula 1 porta(solo mesh, non materiale)

--- PoliTo Explore/Assets/Scripts/BancoRandomiser.cs
+++ PoliTo Explore/Assets/Scripts/BancoRandomiser.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class BancoRandomiser : MonoBehaviour
 {
+    [SerializeField] private float _probBancoA = 0.5f;
+    private Banco[] _banchi;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _banchi = GameObject.FindObjectsOfType<Banco>();
+
+        foreach (Banco b in _banchi)
+        {
+            if(Random.Range(0.0f, 1.0f) < _probBancoA)
+            {
+                b.ApriBanco();
+            }
+        }
     }
 
     // Update is called once per frame
