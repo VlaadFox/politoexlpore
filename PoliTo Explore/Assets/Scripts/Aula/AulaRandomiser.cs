891e2107ea1238a91c11a2638d2dbae88b657ce3
Lorenzo Ercole s277956 <s277956@studenti.polito.i>
2021-02-13 12:11:04 +0100

    aggiunti prefab e spostati script aula(non funzionante, rimuovere navmeshagent da script)

---
+++ PoliTo Explore/Assets/Scripts/Aula/AulaRandomiser.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AulaRandomiser : MonoBehaviour
+{
+    [SerializeField] private float _probBancoA = 0.5f;
+    [SerializeField] private float _probSediaA = 0.5f;
+    [SerializeField] private float _probStudente = 0.5f;
+    public Banco[] banchi;
+    [SerializeField] private SpawnManagerScriptableObject SpawnManager;
+    [SerializeField] private GameObject Cattedra;
+
+    private GameObject[] _objectsToSpawn;
+
+
+    void Start()
+    {
+        banchi = GameObject.FindObjectsOfType<Banco>();
+        _objectsToSpawn = SpawnManager.GetObjectsToSpawn();
+        Randomise();
+        RandomiseStudents();
+        //avg();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Randomise();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ChiudiTutto();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ApriTutto();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+           RandomiseStudents();
+        }
+        
+    }
+    
+    public void RandomiseStudents()
+    {
+        foreach (Banco b in banchi)
+        {
+            if (Random.Range(0.0f, 10.0f)* Vector3.Distance(Cattedra.transform.position, b.transform.position) < _probStudente*65)
+            {
+                int _index = Random.Range(0, _objectsToSpawn.Length - 1);
+                GameObject go = Instantiate(_objectsToSpawn[_index]);
+                go.transform.position = b.transform.position; //TODO: aggiustare posizione davanti al banco 
+                b.ApriSedia();
+            }
+            
+        }
+
+    }
+
+    private void Randomise()
+    {
+
+        foreach (Banco b in banchi)
+        {
+            if (Random.Range(0.0f, 1.0f) < _probBancoA)
+            {
+                b.ApriBanco();
+            }
+            //if (Random.Range(0.0f, 1.0f) < _probSediaA) //messo per prova, penso vada tolto
+            //{
+            //    b.ApriSedia();
+            //}
+        }
+    }
+
+    private void ApriTutto() //Boris reference?
+    {
+        foreach (Banco b in banchi)
+        {
+            b.ApriBanco();
+            b.ApriSedia();
+        }
+    }
+
+    private void ChiudiTutto() 
+    { 
+        foreach (Banco b in banchi)
+        {
+            b.ChiudiBanco();
+            b.ChiudiSedia();
+        }
+    }
+
+   
+}
---
+++ PoliTo Explore/Assets/Scripts/Aula/Professore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Professore : MonoBehaviour
+{
+
+    private Animator _animator;
+    float cont = 3.733f;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAnimations();
+    }
+
+    private void UpdateAnimations()
+    {
+        
+        if (cont > 0f)
+        {
+            _animator.SetBool("talk", false);
+            cont -= Time.deltaTime;
+        }
+        else
+        {
+            _animator.SetBool("talk", true);
+            cont = 3.733f;
+        }
+    }
+}
