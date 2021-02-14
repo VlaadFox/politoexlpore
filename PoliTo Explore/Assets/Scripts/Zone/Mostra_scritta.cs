e6babfa98ec0a2d6b23e29f4ddad09ed29b81e53
Giorgio <giorgios94@hotmail.it>
2021-02-14 01:47:31 +0100

    Aperte porte cortile, aggiunte scritte ingresso zone
    Animazione scritte volendo migliorabile

---
+++ PoliTo Explore/Assets/Scripts/Zone/Mostra_scritta.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mostra_scritta : MonoBehaviour
+{
+    public GameObject uIObject, scritta;
+    Animator scrittaM;
+    public bool start = false;
+    void Start()
+    {
+        uIObject.SetActive(false);
+
+        scrittaM = scritta.GetComponent<Animator>();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            uIObject.SetActive(true);
+            StartCoroutine("Wait");
+            Create(true);
+        }
+    }
+        IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(2);
+            uIObject.SetActive(false);
+        // Destroy(uIObject);
+        // Destroy(gameObject);
+    }
+    void Create(bool state)
+    {
+        scrittaM.SetBool("slide", state);
+        
+    }
+
+}
