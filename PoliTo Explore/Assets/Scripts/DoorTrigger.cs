7ab141952c0cab46db10381cb70bb874e1eeb9f6
Lorenzo Ercole s277956 <s277956@studenti.polito.i>
2021-02-16 11:36:28 +0100

    Apertura porte aule e cambio scena (non asincrono)

--- PoliTo Explore/Assets/Scripts/DoorTrigger.cs
+++ PoliTo Explore/Assets/Scripts/DoorTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorTrigger : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class DoorTrigger : MonoBehaviour
     [SerializeField] private bool _openOnEnter = true;
     [SerializeField] private bool _closeOnExit = true;
 
+    [SerializeField] private Scene Scene;
     private void OnTriggerEnter(Collider other)
     {
         Vector3 othersPositionRelativeToDoor = (other.transform.position - transform.position).normalized;
@@ -21,6 +23,12 @@ public class DoorTrigger : MonoBehaviour
         if (_door != null && _openOnEnter)
             _door.OpenDoor(doorRotation);
 
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            //StartCoroutine(LoadYourAsyncScene());
+            SceneManager.LoadScene("Aula_1porta", LoadSceneMode.Single);
+        }
+
         Debug.Log("Entrato nell'area della porta");
     }
 
@@ -29,5 +37,18 @@ public class DoorTrigger : MonoBehaviour
         if (_door != null && _closeOnExit)
             _door.CloseDoor();
         Debug.Log("Uscito dall'area della porta");
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Aula_1porta");
+
+        
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            Debug.Log("Caricamento Scena");
+        }
     }
 }
