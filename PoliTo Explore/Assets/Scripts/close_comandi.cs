d3c0c95e098796b63efb0b3b5c8293e01255c8b8
Giorgio <giorgios94@hotmail.it>
2021-02-16 01:51:45 +0100

    Risolto problema "esc" quando si ènella pagina comandi

---
+++ PoliTo Explore/Assets/Scripts/close_comandi.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class close_comandi : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI, canvas, player;
+
+    [SerializeField] private bool isPaused;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenuUI.activeInHierarchy == true)
+        {
+            DeactivateMenu();
+        }
+    }
+
+/*   void ActiveteMenu()
+    {
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        canvas.SetActive(false);
+        player.SetActive(false);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+*/
+    public void DeactivateMenu()
+    {
+
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+        canvas.SetActive(true);
+        player.SetActive(true);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
