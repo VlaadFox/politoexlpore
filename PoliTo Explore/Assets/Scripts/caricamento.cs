a616fca9e3d55a5d61f5ceb7d67f90703612d127
Giorgio <giorgios94@hotmail.it>
2021-02-17 01:26:54 +0100

    inizio

---
+++ PoliTo Explore/Assets/Scripts/caricamento.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class caricamento : MonoBehaviour
+{
+    public Animator anim;
+    private int levelToLoad;
+    void Update()
+    {
+
+        
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        Create(true);
+    }
+
+    void Create(bool state)
+    {
+        anim.SetBool("slide", state);
+
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
