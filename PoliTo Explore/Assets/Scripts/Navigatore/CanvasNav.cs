7dbee8a919d796834a7d6ac3385ddf9ee20ac00d
Giorgio <giorgios94@hotmail.it>
2021-02-26 00:00:36 +0100

    Navigatore funzionante uscendo dall'aula, niente più Nazi studente e minimap dentro aula

---
+++ PoliTo Explore/Assets/Scripts/Navigatore/CanvasNav.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasNav : MonoBehaviour
+{
+    private static CanvasNav _instance;
+
+    public static CanvasNav Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            //Debug.Log($"Distrutto player nella posizione {this.transform.position}");
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
